057eeae5da6f5054c5fe748c6ae389657d2f5a9d
Rich_Testardi <rich@testardi.com>
2011-12-30 12:36:34 -0700

    remove nul chars from ut file output

--- test/test3.sh
+++ test/test3.sh
@@ -3,6 +3,6 @@
 if [ X${1:-} = X-u ]; then
   cp basic3.log basic3.txt
 else
-  sh basic3.sh ${1:-} | sed 's/\(v[0-9][0-9.]*\)[a-z]!/\1!/' >basic3.log
+  sh basic3.sh ${1:-} | sed 's/\(v[0-9][0-9.]*\)[a-z]!/\1!/' | tr -d '\000' >basic3.log
   diff -a basic3.txt basic3.log
 fi
